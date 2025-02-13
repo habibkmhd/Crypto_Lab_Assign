c711edda4e2303479e0bb56ddbcd732b12b6e273
GarancePerrot <115420755+GarancePerrot@users.noreply.github.com>
2025-02-13 10:27:00 +0100

    Update main.py
    updated for TAs texts

--- labB/main.py
+++ labB/main.py
@@ -1,5 +1,6 @@
 import os
 import re
+import random
 from src.ngram import NGram
 from src.key import calc_ic, get_key_len, find_key
 from src.decrypt import decrypt
@@ -11,7 +12,16 @@ MAX_KEY_LEN = 16  # Maximum key length for decryption
 # Get the absolute path of the current file and define the ciphertext file path
 path = os.path.abspath(__file__)
 path = os.path.dirname(path)
-CIPHERTEXT_PATH = path + "/ciphertext/vig_group2.crypto"
+# To decrypt texts from students:
+n = random.choice([2,3,5,6,7,9,10,12,17,18,19,24]) #choose a text at random to decrypt
+CIPHERTEXT_PATH = path + "/ciphertext/vig_group"+str(n)+".crypto"
+
+# To decrypt texts from the TAs (replace the line 'key_len =  est_key_len(ciphertext, MAX_KEY_LEN)' by 'key_len = 123')
+# since we found by 'klen.py' that the key_len should be 123
+# now we also found that the key is 'HÄRPRESENTERARUPPSALAUNIVERSITETFORSKKINGMEDUTGÅNGSPUNKTFRÅNENAVUNIVERSITETETSMESTBERÖMDAPROFESSORERGENOMTIDERNACARLVONLINN'
+# so we can skip everything and directly go to 'msg = decrypt(ciphertext, key, ALPHABET)' with 'bestkey' the key above
+#m=random.choice([1,2,3,4,5,6])
+#CIPHERTEXT_PATH = path + "/ciphertext/"+ str(m)+".crypto"
 
 # Load n-gram frequency models for trigrams and quadgrams
 TRIGRAM = NGram(path + "/letter_freqs/se_trigrams.txt", ALPHABET)
@@ -24,6 +34,8 @@ def main():
 
     # STEP 2: Estimate key length using the Friedman test
     key_len = get_key_len(ciphertext, MAX_KEY_LEN)
+    # If decrypting TAs' texts, use: 
+    #key_len = 123
 
     # STEP 3: Guess the encryption key using n-gram analysis
     key = find_key(ciphertext, key_len, ALPHABET, TRIGRAM, QGRAM)
